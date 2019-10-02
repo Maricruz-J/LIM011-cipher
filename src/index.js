16cd237b8b2317c99a1037199a4c3ef5c9d28f8f
Maricruz-J <maricruz.enriquerivera@gmail.com>
2019-10-02 11:13:34 -0500

    html & index

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const nameInformant = document.getElementById('name-informant');
+const contactNumber = document.getElementById('contact-number');
+const accountNumber = document.getElementById('account-number');
+const offsetInformant = document.getElementById('offset-informant');
+
+const nameDenounced = document.getElementById('name-denounced');
+const selectCrime = document.getElementById('select-crime');
+const btnEncode = document.getElementById('btn-encode');
+const dataEncode =  document.getElementById('data-encode');
+
+const nameBeneficiary = document.getElementById('name-beneficiary');
+const offsetDenounced = document.getElementById('offset-denounced');
+const btnDecode  = document.getElementById('btn-decode');
+const dataDecode = document.getElementById('data-decode');
+
