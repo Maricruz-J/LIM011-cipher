204636cc8f04197f3c3fc3fbdb6f193f4b7e8d53
Maricruz-J <maricruz.enriquerivera@gmail.com>
2019-10-07 10:33:23 -0500

    Descifrado OK

--- src/cipher.js
+++ src/cipher.js
@@ -1,92 +1,59 @@
 let positionAscii;
-let positionAsciiEncode;
-let charPositionAsciiEncode;
-let newData = '';
+let positionAsciiChanged;
+let charPositionAsciiChanged;
+let newDataEncode = '';
+let newDataDecode = '';
 
-const encode = (name, contact, account, offset) => {
-
-  for (let i = 0; i < name.length; i++) {
-    positionAscii = name.charCodeAt(i);
-
-    if (positionAscii >= 65 && positionAscii <= 90) {
-      positionAsciiEncode = (positionAscii - 65 + parseInt(offset)) % 26 + 65;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 97 && positionAscii <= 122) {
-      positionAsciiEncode = (positionAscii - 97 + parseInt(offset)) % 26 + 97;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 48 && positionAscii <= 57) {
-      positionAsciiEncode = (positionAscii - 47 + parseInt(offset)) % 10 + 47;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 32 && positionAscii <= 47) {
-      positionAsciiEncode = (positionAscii - 32 + parseInt(offset)) % 16 + 32;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else {
-      console.log(positionAscii);
-      newData += String.fromCharCode(positionAscii);
+const encode = (name, contact, account, offsetInformant) => {
+    newDataEncode = '';
+    let abc = name + ' ' + contact + ' ' + account;
+    for (let i = 0; i < abc.length; i++) {
+        positionAscii = abc.charCodeAt(i);
+        if (positionAscii >= 65 && positionAscii <= 90) {
+            positionAsciiChanged = (positionAscii - 65 + parseInt(offsetInformant)) % 26 + 65;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataEncode += charPositionAsciiChanged;
+        } else if (positionAscii >= 97 && positionAscii <= 122) {
+            positionAsciiChanged = (positionAscii - 97 + parseInt(offsetInformant)) % 26 + 97;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataEncode += charPositionAsciiChanged;
+        } else if (positionAscii >= 48 && positionAscii <= 57) {
+            positionAsciiChanged = (positionAscii - 48 + parseInt(offsetInformant)) % 10 + 48;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataEncode += charPositionAsciiChanged;
+        } else {
+            newDataEncode += String.fromCharCode(positionAscii);
+        }
+        dataEncode.innerHTML = newDataEncode;
     }
-  }
-  dataEncode.innerHTML = newData;
 }
-
-const decode = (name, offset) => {
-  console.log(name);
-
-  for (let i = 0; i < name.length; i++) {
-    positionAscii = name.charCodeAt(i);
-    console.log(positionAscii);
-
-    if (positionAscii >= 65 && positionAscii <= 90) {
-      positionAsciiEncode = (positionAscii + 65 - parseInt(offset)) % 26 + 65;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 97 && positionAscii <= 122) {
-      positionAsciiEncode = (positionAscii + 97 - parseInt(offset)) % 26 + 97;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 48 && positionAscii <= 57) {
-      positionAsciiEncode = (positionAscii + 47 - parseInt(offset)) % 10 + 47;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else if (positionAscii >= 32 && positionAscii <= 47) {
-      positionAsciiEncode = (positionAscii + 32 - parseInt(offset)) % 16 + 32;
-      charPositionAsciiEncode = String.fromCharCode(positionAsciiEncode);
-      newData += charPositionAsciiEncode;
-    } else {
-      console.log(positionAscii);
-      console.log(newData);
-      
-      newData += String.fromCharCode(positionAscii);
+const decode = (name, offsetDenounced) => {
+    newDataDecode = '';
+    for (let i = 0; i < name.length; i++) {
+        positionAscii = name.charCodeAt(i);
+        if (positionAscii === 32) {
+            newDataDecode += '\n';
+        } else if (positionAscii >= 65 && positionAscii <= 90) {
+            positionAsciiChanged = (positionAscii - 90 - parseInt(offsetDenounced)) % 26 + 90;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataDecode += charPositionAsciiChanged;
+        } else if (positionAscii >= 97 && positionAscii <= 122) {
+            positionAsciiChanged = (positionAscii - 122 - parseInt(offsetDenounced)) % 26 + 122;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataDecode += charPositionAsciiChanged;
+        } else if (positionAscii >= 48 && positionAscii <= 57) {
+            positionAsciiChanged = (positionAscii - 57 - parseInt(offsetDenounced)) % 10 + 57;
+            charPositionAsciiChanged = String.fromCharCode(positionAsciiChanged);
+            newDataDecode += charPositionAsciiChanged;
+        } else {
+            newDataDecode += String.fromCharCode(positionAscii);
+        }
     }
-  }
-  console.log(dataDecode);
-  console.log(newData);
-
-  dataDecode.innerHTML = newData;
-
+    dataDecode.innerHTML = newDataDecode;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /* window.cipher = {
-  cifrar: encode(),
-  descifrar: decode()
+    cifrar: encode(),
+    descifrar: decode()
 }; */
-
--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@ const contactNumber = document.getElementById('contact-number');
 const accountNumber = document.getElementById('account-number');
 const offsetInformant = document.getElementById('offset-informant');
 
-const nameDenounced = document.getElementById('name-denounced');
-const selectCrime = document.getElementById('select-crime');
 const btnEncode = document.getElementById('btn-encode');
-const dataEncode =  document.getElementById('data-encode');
+const dataEncode = document.getElementById('data-encode');
 
 const nameBeneficiary = document.getElementById('name-beneficiary');
 const offsetDenounced = document.getElementById('offset-denounced');
-const btnDecode  = document.getElementById('btn-decode');
+const btnDecode = document.getElementById('btn-decode');
 const dataDecode = document.getElementById('data-decode');
 
-btnEncode.addEventListener('click', ()=>{
-    encode(nameInformant.value, contactNumber.value, accountNumber.value,offsetInformant.value);
+btnEncode.addEventListener('click', () => {
+    encode(nameInformant.value, contactNumber.value, accountNumber.value, offsetInformant.value);
 })
 
-btnDecode.addEventListener('click',()=>{
+btnDecode.addEventListener('click', () => {
     decode(nameBeneficiary.value, offsetDenounced.value);
 })
 
